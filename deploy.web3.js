5edcb029cb3e13435bd9b38139b7a3ca82232ba1
muchirasamwel <muchirasamwel@gmail.com>
2025-01-18 12:26:12 +0300

    * Add a web3js based deployment file to sepolia testnet via infura provider and using mnemonics for account access

---
+++ deploy.web3.js
@@ -0,0 +1,41 @@
+const dotenv = require('dotenv')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const { Web3 } = require('web3')
+const { bytecode, ABI } = require('./compile')
+
+dotenv.config()
+
+const mnemonic = process.env.MNEMONIC
+
+const provider = new HDWalletProvider(
+  mnemonic,
+  'https://sepolia.infura.io/v3/' + process.env.INFURA_KEY
+)
+
+const web3 = new Web3(provider)
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts()
+  const account = accounts[0]
+
+  const balanceWei = await web3.eth.getBalance(account)
+
+  const balanceEther = web3.utils.fromWei(balanceWei, 'ether')
+
+  console.log(`Balance for account ${account}: ${balanceEther} Ether`)
+
+  console.log('Deploying contract...')
+  const inboxContract = await new web3.eth.Contract(ABI)
+    .deploy({
+      data: '0x' + bytecode,
+      arguments: ['Hello test network']
+    })
+    .send({
+      from: account,
+      gas: 1000000
+    })
+
+  console.log('inboxContract deployed..', inboxContract._address)
+}
+
+deploy()
